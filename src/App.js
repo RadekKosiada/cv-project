52860699bd15eccfc66e26994ab8c1a51e292a79
RadekKosiada <kosiadar@gmail.com>
2020-01-12 16:16:28 +0100

    Add json files and import them to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './App.css';
 
+// Data
+//
+import education from './json-files/education.json';
+import work from './json-files/work.json';
+//
+import languages from './json-files/languages.json';
+import strenghts  from './json-files/strenghts.json';
+//
+import skills from './json-files/skills.json';
+//
+import projects  from './json-files/projects.json';
+
+let edu = education;
+console.log(edu);
+
 function App() {
   return (
     <div className="App">
