7ab7018fcf88b441e90e5e33d7ef1f36daf08530
RadekKosiada <kosiadar@gmail.com>
2020-01-19 12:31:52 +0100

    Add url to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ function Header () {
     <div>
       <h1 className="header-name">Radosław Kosiada</h1>
       <h3 className="header-jobtitle">JavaScript Developer</h3>
-      <h3 className="header-location">Berlin</h3>
+      <h5 className="header-location">Berlin</h5>
+      <h5 className="header-url">https://radekkosiada.github.io/</h5>
     </div>
   )
 }
